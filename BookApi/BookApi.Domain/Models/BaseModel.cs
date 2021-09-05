86cacd4d530a56b74b142827d2683529e3704fcf
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-09-05 16:04:28 +0300

    Init project

---
+++ BookApi/BookApi.Domain/Models/BaseModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace BookApi.Domain.Models
+{
+    public class BaseCreatedModel
+    {
+        public DateTime CreatedAt { get; set; }
+        public string CreatedFromIP { get; set; }
+        public string CreatedBy { get; set; }
+    }
+
+    public class BaseEditedModel : BaseCreatedModel
+    {
+        public DateTime LastUpdatedAt { get; set; }
+        public string LastUpdatedFromIP { get; set; }
+        public string LastUpdatedBy { get; set; }
+    }
+
+    public class BaseModel : BaseEditedModel
+    {
+
+    }
+
+    public class BaseModel<T> : BaseModel
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public T Id { get; set; }
+    }
+}
---
+++ BookApi/BookApi.Domain/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace BookApi.Domain.Models
+{
+    public class Book : BaseModel<Guid>
+    {
+        [Required, StringLength(150, MinimumLength = 2)]
+        public string Name { get; set; }
+        public string ISBN { get; set; }
+        public string Authors { get; set; }
+        public string Photo { get; set; }
+    }
+}
